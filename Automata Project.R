5da1eb7a112f0af10b0daf8fdaeca974cc38d474
Jan Grzybowski <grzybowskij@student.mini.pw.edu.pl>
2014-12-14 16:28:22 +0100

    Wszystkie podfunkcje przygotowane pod raport
    Wszystko poza funkcją CreateAutomata i AC2014 przygotowane do działania
    wg specyfikacji.

--- Automata Project.R
+++ Automata Project.R
@@ -1,32 +1,48 @@
 ### SETS FUNCTIONS ###
-GenerateSets <- function (classes, instancesL, instancesT, REJinstances, features, rangeMin, rangeMax, sigma){
-  learningSet = array(,dim = c(instancesL*classes+REJinstances, features))
-  testingSet = array(,dim = c(instancesT*classes+REJinstances, features))
-  for (c in 1:classes){
-    for(f in 1:features){
-      m = mean(runif(instancesL,min=rangeMin,max=rangeMax)) 
-      learningSet[(1:instancesL)+((c-1)*instancesL),f]= m
-      testingSet[(1:instancesT)+((c-1)*instancesT),f]= m      
+GenerateSets <- function (noClasses, noRepetitionsInClass, percTest, percForeign, features, rangeMin, rangeMax, sigma){
+  noLearnWords = noClasses * noRepetitionsInClass
+  noTestWords = percTest/100 * noClasses * noRepetitionsInClass
+  noTrainWords = noLearnWords - noTestWords
+  noForeignLearn =  percForeign/100 * noTrainWords
+  noForeignTest = percForeign/100 * noTestWords
+  noForeignWords = noForeignLearn + noForeignTest
+  
+  learningSet = array(,dim = c(noLearnWords, features+1))
+  for (c in 1:noClasses){
+    indexesOfClass = (1:noRepetitionsInClass)+((c-1)*noRepetitionsInClass);
+    learningSet[indexesOfClass,1] = c
+    for(f in 2:(features+1)){
+      m = mean(runif(noRepetitionsInClass, min=rangeMin, max=rangeMax)) 
+      learningSet[indexesOfClass,f] = m
     }
-  }
-  learningSet = learningSet + rnorm(n=instancesL*classes*features,mean =0, sd = sigma)
-  testingSet = testingSet + rnorm(n=instancesT*classes*features,mean =0, sd = sigma)
-  #for(w in 1:REJinstances){
-  data = runif(REJinstances*features, rangeMin, rangeMax)
-  x = instancesL*classes+1
-  y = instancesL*classes+REJinstances
-  learningSet[x:y,] = data
-  data = runif(REJinstances*features, rangeMin, rangeMax)
-  x = instancesT*classes+1
-  y = instancesT*classes+REJinstances
-  testingSet[x:y,] = data
-  #}
-  list("learn" = learningSet, "test"=testingSet);
+  }  
+  learningSet[,-1] = learningSet[,-1] + rnorm(n=noLearnWords*features, mean=0, sd=sigma)
+  
+  learningSet = learningSet[sample(noLearnWords),]
+  
+  foreignSet= array(runif(n = noForeignWords*(features+1), min = rangeMin, max = rangeMax),dim=c(noForeignWords,features+1))  
+  if(noForeignWords > 0)
+    foreignSet[,1] = 0
+  
+  trainN = learningSet[1:noTrainWords,]
+  testN = learningSet[(noTrainWords+1):noLearnWords,]
+  
+  if(noForeignLearn > 0)
+    trainF = foreignSet[1:noForeignLearn,]
+  else
+    trainF = NULL
+  
+  if(noForeignTest > 0)
+    testF = foreignSet[(noForeignLearn+1):(noForeignWords),]
+  else
+    testF = NULL
+
+  list("learn" = rbind(trainN, trainF, deparse.level = 0), "test" = rbind(testN, testF, deparse.level = 0))
 }
 
 Normalize <- function(sets){
-  columns = dim(sets$learn)[2];
-  for (i in 1:columns){
+  columns = dim(sets$learn)[2]
+  for (i in 2:columns){
     mini = min(min(sets$learn[,i]),min(sets$test[,i]));
     maxi = max(max(sets$learn[,i]),max(sets$test[,i]));
     sets$learn[,i] = (sets$learn[,i]-mini)/(maxi-mini);
@@ -41,10 +57,28 @@ ChangeValuesToSymbols <- function(x,symbols){
   newVals
 }
 ### AUTOMATA FUNCTIONS ###
-CreateTT <- function(states, symbols, nondeterminism = 1){
-  TT = array(data = runif(n = states*states*symbols, min = 0.0, max = 1.0),
-             dim = c(states, states, symbols))
-  round(TT,digits = 3)
+CreateTT <- function(states, symbols, nondeterminism = 1, rejection = TRUE){
+  if(is.infinite(nondeterminism))
+  {
+    TT = array(data = runif(n = states*states*symbols, min = 0.0, max = 1.0),
+                dim = c(states, states, symbols))
+    TT = round(TT,digits = 3)
+  }
+  else
+  {
+    if (rejection)
+      minStates = 0
+    else
+      minStates = 1
+    TT = array(data = 0,dim = c(states, states, symbols))
+    for(symbol in 1:symbols){
+      for(state in 1:states){
+        TT[sample(1:states,sample(minStates:nondeterminism)),state,symbol] = 1;
+      }
+    }
+  }
+  
+  TT
 }
 
 ComputeNextState <- function(TT,inputSymbol,state){
@@ -53,11 +87,6 @@ ComputeNextState <- function(TT,inputSymbol,state){
     newState[i] = UseTriangleNorm(TT[i,,inputSymbol],state)
   }
   newState
-  
-  #if(rej)
-  #  states = sort(unique(c(states,0)))
-  #else
-  #  states = sort(unique(as.vector(states)))
 }
 
 UseTriangleNorm <- function(row, column){
@@ -101,23 +130,33 @@ CalculateSymbolsVector <- function(value,numberOfSymbols){
   round(result,4)
 }
 
-ClassifyWord <- function(TT,word){  
+ClassifyWord <- function(TT,word, discrete){  
   states = dim(TT)[1]
   symbols = dim(TT)[3]
-  state = rep(0.3,states);
-  Ytab = array(dim=c(states,symbols))
-  for(i in 1:length(word)){
-    symV = CalculateSymbolsVector(word[i],states)
-    for(symbol in 1:symbols){
-      Ytab[,symbol] = ComputeNextState(TT,symbol,state);
-    }
-    for(symbol in 1:symbols){
+  if(discrete == TRUE)
+  {
+    state = rep(0,states)
+    state[1] = 1
+    for(i in 1:length(word)){
+      state = CalculateSymbolsVector(word[i],states)
+    }    
+  }
+  else{
+    state = rep(0.3,states);
+    Ytab = array(dim=c(states,symbols))
+    for(i in 1:length(word)){
+      symV = CalculateSymbolsVector(word[i],states)
+      for(symbol in 1:symbols){
+        Ytab[,symbol] = ComputeNextState(TT,symbol,state);
+      }
+      for(symbol in 1:symbols){
+        for(place in 1:states)
+          Ytab[place,symbol]=triMin(c(symV[place],Ytab[place,symbol]))
+      }
       for(place in 1:states)
-        Ytab[place,symbol]=triMin(c(symV[place],Ytab[place,symbol]))
+        state[place] = triMax(Ytab[place,])
+      #cat("\t states: (",state,") \n");
     }
-    for(place in 1:states)
-      state[place] = triMax(Ytab[place,])
-    #cat("\t states: (",state,") \n");
   }
   state
 }
@@ -125,20 +164,19 @@ ClassifyWord <- function(TT,word){
 CalculateError <- function(TT,words,instances,minChance){
   error = 0;
   wordsNo = dim(words)[1]
+  noFeatures = dim(words)[2]-1
   classesNo = dim(TT)[1]
   for(i in 1:(wordsNo)){
     #cat("#",i," word - ", words[i,], " \n");
-    possibleClassification = (ClassifyWord(TT,words[i,]))
-    
+    possibleClassification = (ClassifyWord(TT,words[i,2:noFeatures+1]))
+    expectedClass = words[i,1]
     #If the word should be rejected
-    if (classesNo*instances < i){
+    if (expectedClass==0){
         if(sum(possibleClassification<minChance) != length(possibleClassification))
           error=error+sum(possibleClassification[possibleClassification>minChance]-minChance)
-        expectedClass = 0
     }
     #If the word should be accepted in specific class  
     else {
-      expectedClass = (((i-1)%/%instances)+1)  
       if (possibleClassification[expectedClass] < minChance)
         error=error+(minChance - possibleClassification[expectedClass]);
     }
@@ -152,8 +190,31 @@ CalculateErrorFromVector <- function(vTT,words,instances,states,symbols,minChanc
   CalculateError(TT,words,instances,minChance)
 }
 
-HandlePSOVector <- function(vTT,states,symbols){
-  array(vTT,dim = c(states,states,symbols))
+HandlePSOVector <- function(vTT,states,symbols,rejecting,nondeterminism){
+  TT = array(vTT,dim = c(states,states,symbols))
+  if(is.infinite(nondeterminism) == FALSE)
+    ChangeValuesToZerosAndOnes(TT,rejecting,nondeterminism)
+  else
+    TT
+}
+
+ChangeValuesToZerosAndOnes <- function(TT,rejecting, nondeterminism){
+  states = dim(TT)[1]
+  symbols = dim(TT)[3]
+  if(rejecting == FALSE)
+    minimumPSOValue = 0
+  else
+    minimumPSOValue = 0.2
+  for(layer in 1:symbols)
+    for(column in 1:states)
+    {
+      ord = order(TT[,column,layer], decreasing = TRUE)
+      top = TT[,column,layer][ord[1:nondeterminism]]
+      TT[,column,layer][ord[1:nondeterminism]][top>minimumPSOValue] = -Inf
+    } 
+  TT[!is.infinite(TT)] <- 0;
+  TT[is.infinite(TT)] <- 1;
+  TT
 }
 
 
