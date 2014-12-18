632c80905da46dfa187e858c19b91dc4cde9d1f7
Jan Grzybowski <grzybowskij@student.mini.pw.edu.pl>
2014-12-18 23:13:47 +0100

    Konfiguracja paczek wymaganych do uruchomienia

---
+++ preconfiguration.R
@@ -0,0 +1,8 @@
+if(!require(hydroPSO))
+  install.packages("hydroPSO", dependencies=TRUE)
+if(!require(pso))
+  install.packages("pso", dependencies=TRUE)
+if(!require(openxlsx))
+  install.packages("openxlsx", dependencies = TRUE)
+
+
